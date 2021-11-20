f678ad43a126703e143a3a8125637452af62a90d
suraj9982 <surajsk9982@gmail.com>
2021-11-20 15:05:21 +0530

    [suraj] add : this add item in reverse order

--- DataStructuresComputation/DataStructuresComputation/LinkedList.cs
+++ DataStructuresComputation/DataStructuresComputation/LinkedList.cs
@@ -27,5 +27,33 @@ namespace DataStructuresComputation
             }
             Console.WriteLine("{0} inserted items in linkedlist", node.data);
         }
+        internal void AddInReverseOrder(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newnode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newnode;
+                head.next = temp;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
--- DataStructuresComputation/DataStructuresComputation/Program.cs
+++ DataStructuresComputation/DataStructuresComputation/Program.cs
@@ -6,11 +6,34 @@ namespace DataStructuresComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DATA STRUCTURES");
             LinkedList linked = new LinkedList();
-            linked.Adddata(56);
-            linked.Adddata(30);
-            linked.Adddata(70);
+            Console.WriteLine("DATA STRUCTURES");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("CHOOSE OPTION\n1.Add simple linkedlist\n2.Add In Reverse\n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linked.Adddata(56);
+                        linked.Adddata(30);
+                        linked.Adddata(70);
+                        break;
+                    case 2:
+                        linked.AddInReverseOrder(70);
+                        linked.AddInReverseOrder(30);
+                        linked.AddInReverseOrder(56);
+                        linked.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice of option should be betweeen 1 to 3");
+                        break;
+                }
+            }   
         }
     }
 }
