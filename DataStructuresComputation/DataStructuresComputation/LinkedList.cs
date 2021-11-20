5aa46443519efc6178df8a96a36795dab92b349b
suraj9982 <surajsk9982@gmail.com>
2021-11-20 14:38:01 +0530

    [suraj] add : create a new linkedlist

---
+++ DataStructuresComputation/DataStructuresComputation/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresComputation
+{
+   public class LinkedList
+    {
+        internal Node head;
+        public void Adddata(int data)
+        {
+            Node node = new Node(data);
+             if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted items in linkedlist", node.data);
+        }
+    }
+}
---
+++ DataStructuresComputation/DataStructuresComputation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataStructuresComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DATA STRUCTURES");
+            LinkedList linked = new LinkedList();
+            linked.Adddata(56);
+            linked.Adddata(30);
+            linked.Adddata(70);
+        }
+    }
+}
