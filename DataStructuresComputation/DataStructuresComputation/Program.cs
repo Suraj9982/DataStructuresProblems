c00c628f9491f0cb99239387af13cc5424a35c4b
suraj9982 <surajsk9982@gmail.com>
2021-11-21 20:19:41 +0530

    [suraj] add  : this function insert data in queue

--- DataStructuresComputation/DataStructuresComputation/Program.cs
+++ DataStructuresComputation/DataStructuresComputation/Program.cs
@@ -8,11 +8,12 @@ namespace DataStructuresComputation
         {
             LinkedList linked = new LinkedList();
             StackOperations stack = new StackOperations();
+            QueueOperation queue = new QueueOperation();
             Console.WriteLine("DATA STRUCTURES");
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nCHOOSE OPTION\n1.Add simple linkedlist\n2.Add In Reverse\n3.Insert At Particular postion\n4.Remove First Node\n5.RemoveLastNode\n6.Push into Stack\n7.Peek into Stack\n8.Pop from stack\n9.Exit");
+                Console.WriteLine("\nCHOOSE OPTION\n1.Add simple linkedlist\n2.Add In Reverse\n3.Insert At Particular postion\n4.Remove First Node\n5.RemoveLastNode\n6.Push into Stack\n7.Peek into Stack\n8.Pop from stack\n9.Inser into Queue\n10.exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -55,6 +56,11 @@ namespace DataStructuresComputation
                         stack.Peek();
                         break;
                     case 9:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        break;
+                    case 10:
                         flag = false;
                         break;
                     default:
---
+++ DataStructuresComputation/DataStructuresComputation/QueueOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresComputation
+{
+    class QueueOperation
+    {
+        Node front;
+        Node rear;
+        public QueueOperation()
+        {
+            this.front = this.rear = null;
+        }
+        internal void Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.rear == null)
+            {
+                this.front = this.rear = newnode;
+            }
+            else
+            {
+                this.rear.next = newnode;
+                this.rear = newnode;
+            }
+            Console.WriteLine("{0} is inserted into queue", newnode.data);
+        }
+    }
+
+
+}
